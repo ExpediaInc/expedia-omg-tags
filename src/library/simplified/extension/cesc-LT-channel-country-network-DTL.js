31dcfb43fe881d8bc2aa43a4849e397131eeb123
Trung Nguyen <trunguyen@BELC02R3FSYG8WN.sea.corp.expecn.com>
2017-03-15 17:51:13 -0700

    fix undefined fields for cesc

--- src/library/simplified/extension/cesc-LT-channel-country-network-DTL.js
+++ src/library/simplified/extension/cesc-LT-channel-country-network-DTL.js
@@ -5,6 +5,9 @@ var priorTimeStamp = '0';
 var priorMarketingCode = "";
 var length = marketingChannels.length;
 
+
+// Old logic that goes through cesc fields to find last touch and then set DTL.
+
 for(var i=0; i < length; i++){
     var currentTimeStamp = b["context.marketingAttribution."+marketingChannels[i]+"Timestamp"];
     var currentMarketingCode = b["context.marketingAttribution."+marketingChannels[i]];
@@ -17,34 +20,44 @@ for(var i=0; i < length; i++){
     }
 }
 
-b["cescLastTouchTimeStamp"] = lastTouchTimeStamp;
-b["cescLastTouchMarketingCode"] = lastTouchMarketingCode;
+
 b["cescPriorTouchTimeStamp"] = priorTimeStamp;
 b["cescPriorTouchMarketingCode"] = priorMarketingCode;
 
+var fullMarketingCode = lastTouchMarketingCode;
+var patrialMarketingCode = b["context.marketingAttribution.campaignId"];
 
-var dateDifference = parseInt((new Date() - new Date(lastTouchTimeStamp))/(1000*60*60*24));
+b["cescDTL"] = "";
+if(fullMarketingCode != undefined && patrialMarketingCode != undefined && fullMarketingCode.indexOf(patrialMarketingCode) > -1 ){
+    var marketingCodeAndDTL = fullMarketingCode.split(patrialMarketingCode + ".");
+    if( marketingCodeAndDTL.length >= 2 ){
+        b["cescDTL"]  = marketingCodeAndDTL[1];
+    }
+}
+
+
+// Use CID for last touch
+
+b["cescLastTouchTimeStamp"] = b["context.marketingAttribution.campaignIdTimestamp"];
+b["cescLastTouchMarketingCode"] = b["context.marketingAttribution.campaignId"];
+var dateDifference = parseInt((new Date() - new Date(b["cescLastTouchTimeStamp"]))/(1000*60*60*24));
+
+b['cescLastTouch_isexpired'] = "";
 if(dateDifference <= 30 && dateDifference >= 0) {
     b['cescLastTouch_isexpired'] = false;
 }else {
     b['cescLastTouch_isexpired'] = true;
 }
 
-if(lastTouchMarketingCode.split(".").length >= 4){
-    b["cescChannel"] = lastTouchMarketingCode.split(".")[0];
-    b["cescCompany"] = lastTouchMarketingCode.split(".")[1];
-    b["cescCountry"] = lastTouchMarketingCode.split(".")[2];
-    b["cescNetwork"] = lastTouchMarketingCode.split(".")[3];
-}
-
-
-var fullMarketingCode = lastTouchMarketingCode;
-var patrialMarketingCode = b["context.marketingAttribution.campaignId"];
-
-if(fullMarketingCode != undefined && patrialMarketingCode != undefined && fullMarketingCode.indexOf(patrialMarketingCode) > -1 ){
-    var marketingCodeAndDTL = fullMarketingCode.split(patrialMarketingCode + ".");
-    if( marketingCodeAndDTL.length >= 2 ){
-        b["cescDTL"]  = marketingCodeAndDTL[1];
-        b["cescLastTouchMarketingCode"] = patrialMarketingCode;     //This is to handle the case where there is DTL in the lastTouchMarketingCode.
-    }
+// Load rule looks for country and network, and if these are undefined in the udo the load rule will break and tag will not fire even it other conditions are met.
+// Therefore, initializing it to empty so load rule doesn't break.
+b["cescChannel"] = "";
+b["cescCompany"] = "";
+b["cescCountry"] = "";
+b["cescNetwork"] = "";
+if(b["cescLastTouchMarketingCode"].split(".").length >= 4){
+    b["cescChannel"] = b["cescLastTouchMarketingCode"].split(".")[0];
+    b["cescCompany"] = b["cescLastTouchMarketingCode"].split(".")[1];
+    b["cescCountry"] = b["cescLastTouchMarketingCode"].split(".")[2];
+    b["cescNetwork"] = b["cescLastTouchMarketingCode"].split(".")[3];
 }
