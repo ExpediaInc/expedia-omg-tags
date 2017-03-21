12d5ca876fae6d91959c0544c0feed341f7d1379
cmschell <christina.m.schell@tealium.com>
2017-03-21 08:54:52 -0700

    Commit user segmentation and GTM iframe
    Added GTM iframe custom container to partnerships and updated user
    segmentation extension in Travelocity

---
+++ src/profile/partnerships/tag/gtm-iframe-149.js
@@ -0,0 +1,83 @@
+//~~tv:20010.20140827
+//~~tc: Tealium Custom Container - GTM iFrame
+
+//tealium universal tag - utag.sender.custom_container ut4.0.##UTVERSION##, Copyright ##UTYEAR## Tealium.com Inc. All Rights Reserved.
+try {
+  (function (id, loader) {
+    var u = {};
+    utag.o[loader].sender[id] = u;
+
+    // Start Tealium loader 4.32
+    // Please do not modify
+    if (utag === undefined) { utag = {}; } if (utag.ut === undefined) { utag.ut = {}; } if (utag.ut.loader === undefined) { u.loader = function (o) { var a, b, c, l; a = document; if (o.type === "iframe") { b = a.createElement("iframe"); b.setAttribute("height", "1"); b.setAttribute("width", "1"); b.setAttribute("style", "display:none"); b.setAttribute("src", o.src); } else if (o.type === "img") { utag.DB("Attach img: " + o.src); b = new Image(); b.src = o.src; return; } else { b = a.createElement("script"); b.language = "javascript"; b.type = "text/javascript"; b.async = 1; b.charset = "utf-8"; b.src = o.src; } if (o.id) { b.id = o.id; } if (typeof o.cb === "function") { if (b.addEventListener) { b.addEventListener("load", function () { o.cb(); }, false); } else { b.onreadystatechange = function () { if (this.readyState === "complete" || this.readyState === "loaded") { this.onreadystatechange = null; o.cb(); } }; } } l = o.loc || "head"; c = a.getElementsByTagName(l)[0]; if (c) { utag.DB("Attach to " + l + ": " + o.src); if (l === "script") { c.parentNode.insertBefore(b, c); } else { c.appendChild(b); } } }; } else { u.loader = utag.ut.loader; }
+    // End Tealium loader
+
+    u.ev = {'view' : 1};
+
+    u.initialized = false;
+
+    ##UTGEN##
+
+    u.send = function(a, b) {
+      if (u.ev[a] || u.ev.all !== undefined) {
+        //##UTENABLEDEBUG##utag.DB("send:##UTID##");
+
+        var c, d, e, f, i;
+
+        u.data = {
+          "account_id": "",
+          "base_url": ""
+        };
+
+
+        /* Start Tag-Scoped Extensions Code */
+        /* Please Do Not Edit This Section */
+        ##UTEXTEND##
+        /* End Tag-Scoped Extensions Code */
+
+
+        /* Start Mapping Code */
+        for (d in utag.loader.GV(u.map)) {
+          if (b[d] !== undefined && b[d] !== "") {
+            e = u.map[d].split(",");
+            for (f = 0; f < e.length; f++) {
+              u.data[e[f]] = b[d];
+            }
+          }
+        }
+        /* End Mapping Code */
+
+
+        /* Start Tag Sending Code */
+
+        u.data.account_id = u.data.account_id || "GTM-KNLCXK"; // default is AirNZ
+        u.data.base_url = u.data.base_url || "//www.googletagmanager.com/ns.html"; // default base url
+
+
+        (function () {
+            var url = (u.data.base_url +"?id=" + u.data.account_id);
+            var gtm = document.createElement('iframe');
+            gtm.style.width = "1px";
+            gtm.style.height = "1px";
+            gtm.style.display = "none";
+            gtm.async = true;
+            gtm.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + url;
+            var s = document.body.firstChild;
+            s.parentNode.insertBefore(gtm, s);
+        })();
+
+
+        /* End Tag Sending Code */
+
+
+
+        //##UTENABLEDEBUG##utag.DB("send:##UTID##:COMPLETE");
+      }
+    };
+    utag.o[loader].loader.LOAD(id);
+  })("##UTID##", "##UTLOADERID##");
+} catch (error) {
+  utag.DB(error);
+}
+//end tealium universal tag
+
